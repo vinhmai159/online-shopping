150d0e50a59926d242235164fc6f2d992659e882
Kien Tran <kien868788@gmail.com>
2019-10-29 21:25:58 +0700

    refactor responseBuilder

--- src/packages/user/controller.js
+++ src/packages/user/controller.js
@@ -2,27 +2,31 @@ import service from './service'
 import responseBuilder from  '../../utils/responseBuilder'
 import to from '../../utils/to'
 
+const { error: errorCode, success: successCode } = responseBuilder.statusCode;
+const { successMessage, notFoundMessage } = responseBuilder.message;
+const buildRes = responseBuilder.build;
+
 async function register(req, res) {
   const [error, user] = await to(service.register(req.body))
   if (error) {
-    res.jsonp(responseBuilder.build(responseBuilder.statusCode.error, {}, error.message))
+    res.jsonp(buildRes(errorCode, {}, error.message))
   }
-  res.jsonp(responseBuilder.build(responseBuilder.statusCode.success, user, responseBuilder.message.success))
+  res.jsonp(buildRes(successCode, user, successMessage))
 }
 
 async function login(req, res) {
   const [error, user] = await to(service.login(req.body))
   if (error) {
-    res.jsonp(responseBuilder.build(responseBuilder.statusCode.error, {}, error.message))
+    res.jsonp(buildRes(errorCode, {}, error.message))
   }
   if (!user) {
-    res.jsonp(responseBuilder.build(responseBuilder.statusCode.notFound, {}, responseBuilder.message.notFound))
+    res.jsonp(buildRes(responseBuilder.statusCode.notFound, {}, notFoundMessage))
   }
-  res.jsonp(responseBuilder.build(responseBuilder.statusCode.success, {user: user}, responseBuilder.message.success))
+  res.jsonp(buildRes(successCode, {user: user}, successMessage))
 }
 
 async function getCurrent(req, res) {
-    res.jsonp(responseBuilder.build(responseBuilder.statusCode.success, req.user, responseBuilder.message.success));
+    res.jsonp(buildRes(successCode, req.user, successMessage));
 }
 
 export default {
